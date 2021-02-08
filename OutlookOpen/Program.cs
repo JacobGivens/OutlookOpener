6ebc61e46513713adf97306df7bf0b0ac3d8d6b4
Jacob Givens <jacob.givens@rockets.utoledo.edu>
2021-02-08 16:42:19 -0500

    Initial Commit

---
+++ OutlookOpen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace OutlookOpen {
+	class Program {
+		static void Main(string[] args) {
+			Thread.Sleep(20000);
+			do {
+				Process[] outlookProcess = Process.GetProcessesByName("OUTLOOK");
+				if (outlookProcess.Length == 0) {
+					Process.Start("C:\\Program Files\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE");
+					Thread.Sleep(30000);
+				} else {
+					Thread.Sleep(10000);
+				}
+			} while (true);
+		}
+	}
+}
